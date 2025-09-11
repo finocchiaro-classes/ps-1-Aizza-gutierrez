efa9be18f32d68e3b1779689c859f93787971b8e
Aizza-gutierrez <gutieai@bc.edu>
2025-09-10 23:58:12 -0400

    Update ps1-part4.py

--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,18 @@
+# Ask the user for inputs
+prior_arrests = int(input("Prior arrests: ") )
+local_ordinance = input("Prior arrests for local orginance (Y/N): ") 
+age = int(input("Age at release: ") )
 
+## start with the number of prior arrests
+risk_score = prior_arrests
+
+# subtract 1 if no prior arrests for a local ordinance
+if local_ordinance.upper() == "N":
+    risk_score -= 1
+
+# subtract 1 if age at release is greater than 40
+if age > 40:
+    risk_score -= 1
+
+## result reslute
+print(f'The recidivism risk score is {risk_score}')
