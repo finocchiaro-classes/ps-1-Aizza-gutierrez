93766f82e9579b487bde4357f6abb55fdd6d2feb
Aizza-gutierrez <gutieai@bc.edu>
2025-09-10 23:18:29 -0400

    Update ps1-part2.py

--- ps1-part2.py
+++ ps1-part2.py
@@ -1,11 +1,17 @@
+# So this function should do everything that part 1 did, except we will create a function that does the math
 # Write a function that takes two variables and does all the computations asked
-
-# Ask the user for the first number, store the value in a variable
-
+def number_fun(a,b):
+# Repeat back the numbers
+  print(f' A:{a} and B:{b}')
+  #Perform calculations. Be careful about string formatting for autograders.
+  print (int(a) + int(b) )   # this is the addition  
+  print (int(a) * int(b) ) # this is the multiplication 
+  print (int(a) ** int(b) ) # this is the exponential
+  print (int(a) % int(b) ) # this is the modulus   
+  
+#This part asks the user for the first number, stores the value in a variable
+firstnum = input("Enter an integer between 10 and 100") 
 
 # Ask the user for the second number, store the value in a variable
-
-
-# Repeat back the numbers
-
-# Perform calculations. Be careful about string formatting for autograders.
+secondnum = input("Enter another integer that is less than 4") 
+number_fun(firstnum, secondnum) 
