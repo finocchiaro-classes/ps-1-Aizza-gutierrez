b854fd80cc5709da94bb82c08e1567a7a08e0920
Aizza-gutierrez <gutieai@bc.edu>
2025-09-10 22:53:30 -0400

    Update ps1-part1.py

--- ps1-part1.py
+++ ps1-part1.py
@@ -1,6 +1,5 @@
 print("\n")
 # Ask the user for the first number, store the value in a variable
-<<<<<<< HEAD
 firstnum = input("Enter an integer between 10 and 100")
 
 # Ask the user for the second number, store the value in a variable
@@ -10,8 +9,6 @@ secondnum= input("Enter an integer less than 4")
 print(f'First number: {firstnum}and Second number:{secondnum}') 
 
 # Perform calculations. Be careful about string formatting for autograders.
-
-=======
 firstnum = input("Enter an integer between 10 and 100") 
 
 
@@ -22,13 +19,8 @@ secondnum = input("Enter an integer less than 4")
 print(f'First number: {firstnum}and Second number:{secondnum}')
 
 # Perform calculations. Be careful about string formatting for autograders.
->>>>>>> 2216052e9eb71b1afe8d3d8a7d2c6211ebaa09b5
-print (int(firstnum) + int(secondnum)) 
-print (int(firstnum) * int(secondnum)) 
-print (int(firstnum) ** int(secondnum)) 
-print (int(firstnum) % int(secondnum)) 
-<<<<<<< HEAD
+print (int(firstnum) + int(secondnum)) # this is the addition  
+print (int(firstnum) * int(secondnum)) # this is the multiplication 
+print (int(firstnum) ** int(secondnum)) # this is the exponential
+print (int(firstnum) % int(secondnum)) # this is the modulus 
 
-
-=======
->>>>>>> 2216052e9eb71b1afe8d3d8a7d2c6211ebaa09b5
