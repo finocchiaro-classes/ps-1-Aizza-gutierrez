6857f6ce1079797912b275d366b126e8ba8ef576
Aizza Gutierrez <gutieai@bc.edu>
2025-09-10 22:50:33 -0400

    Update ps1-part1.py

--- ps1-part1.py
+++ ps1-part1.py
@@ -1,11 +1,18 @@
+print("\n")
 # Ask the user for the first number, store the value in a variable
-print('hi')
-
+firstnum = input("Enter an integer between 10 and 100")
 
 # Ask the user for the second number, store the value in a variable
-
+secondnum= input("Enter an integer less than 4")
 
 # Repeat back the numbers
-
+print(f'First number: {firstnum}and Second number:{secondnum}') 
 
 # Perform calculations. Be careful about string formatting for autograders.
+
+print (int(firstnum) + int(secondnum)) 
+print (int(firstnum) * int(secondnum)) 
+print (int(firstnum) ** int(secondnum)) 
+print (int(firstnum) % int(secondnum)) 
+
+
